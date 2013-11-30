6c15b8e5d863f094c1ed94cb83fd25ac42e435f8
Jeremy Coulon <jeremy.coulon@free.fr>
2013-11-30 21:12:37 +0100

    Use git describe to obtain current project version

--- version.cmake
+++ version.cmake
@@ -6,3 +6,18 @@ set(${PROJECT_NAME}_PATCH 3)
 set(${PROJECT_NAME}_TWEAK 4)
 
 set(${PROJECT_NAME}_VERSION "${${PROJECT_NAME}_MAJOR}.${${PROJECT_NAME}_MINOR}.${${PROJECT_NAME}_PATCH}.${${PROJECT_NAME}_TWEAK}")
+find_package(Git)
+if(GIT_FOUND)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_STRING
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    string(FIND ${${PROJECT_NAME}_VERSION_STRING} ${${PROJECT_NAME}_VERSION} substring_found)
+    if(substring_found EQUAL -1)
+        message(FATAL_ERROR "Version mismatch: ${${PROJECT_NAME}_VERSION_STRING} != ${${PROJECT_NAME}_VERSION}")
+    endif()
+else()
+    set(${PROJECT_NAME}_VERSION_STRING "v${${PROJECT_NAME}_VERSION}")
+endif()
